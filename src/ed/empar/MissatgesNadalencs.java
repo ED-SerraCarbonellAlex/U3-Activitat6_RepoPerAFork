f92cd0a3ed528f970f6565018b70f677a96b9e03
Alex Serra Carbonell <alexsc7000@gmail.com>
2022-12-20 11:02:56 +0100

    [NEW] BON NADAL

--- src/ed/empar/MissatgesNadalencs.java
+++ src/ed/empar/MissatgesNadalencs.java
@@ -173,6 +173,12 @@ public class MissatgesNadalencs {
 	}
 
 	private static void AlumneFila3PC6() {
+                System.out.println("====================================");
+                System.out.println("Hola, sóc EMPAR la professora de ED");
+                System.out.println(" BON NADAL, Salut i Pau per a tots.");
+                System.out.println(" FELIÇ ANY NOU !!!!!!");
+                System.out.println("====================================");
+                System.out.println();
 
 	}
 
